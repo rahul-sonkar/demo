efe887cd5caca9faf35c229a641cb9ae9ed6ad11
Rahul Sonkar <info@rahul.com>
2021-12-01 14:16:38 +0530

    v1 of rate.c

---
+++ rate.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main(void)
+{
+    double number, gst = 0, value = 0;
+    printf("enter rate : ");
+    scanf("%lf", &number);
+    while (1)
+    {
+        if (value + (value * 0.12) >= number)
+        {
+            printf("value is %.2lf\ngst is %.2lf\n", value, value * 0.12);
+            break;
+        }
+        value++;
+    }
+}

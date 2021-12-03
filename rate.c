e3181a5861f3e72a388f521f378a35040d9673b6
Rahul Sonkar <contect@main-pc.com>
2021-12-03 20:39:34 +0530

    faild to do

--- rate.c
+++ rate.c
@@ -2,14 +2,42 @@
 #include <stdio.h>
 void main(void)
 {
-    double number, gst = 0, value = 0;
-    printf("enter rate : ");
+    double number, value = 0;
+    int gst;
+    printf("enter rate : \n");
     scanf("%lf", &number);
-    while (1)
+    printf("enter gst %%\n");
+    scanf("%d", &gst);
+    for (;;)
     {
-        if (value + (value * 0.12) >= number)
+        switch (gst)
         {
-            printf("value is %.2lf\ngst is %.2lf\n", value, value * 0.12);
+        case '5':
+            if (value + (value * 0.05) >= number)
+            {
+                printf("value is %.2lf\ngst is %.2lf\n", value, value * 0.05);
+            }
+            break;
+
+        case 12:
+            if (value + (value * 0.12) >= number)
+            {
+                printf("value is %.2lf\ngst is %.2lf\n", value, value * 0.12);
+            }
+            break;
+
+        case 18:
+            if (value + (value * 0.18) >= number)
+            {
+                printf("value is %.2lf\ngst is %.2lf\n", value, value * 0.18);
+            }
+            break;
+
+        case 28:
+            if (value + (value * 0.28) >= number)
+            {
+                printf("value is %.2lf\ngst is %.2lf\n", value, value * 0.28);
+            }
             break;
         }
         value++;

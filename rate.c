e8da10aae2926b8eaba0315663a87c3e62d5c03f
Rahul Sonkar <info@rahul.com>
2021-12-03 20:37:04 +0530

    final version of rate.c

--- rate.c
+++ rate.c
@@ -1,17 +1,35 @@
 // this is new
 #include <stdio.h>
+void values();
 void main(void)
 {
-    double number, gst = 0, value = 0;
-    printf("enter rate : ");
-    scanf("%lf", &number);
+    int flag = 0;
+
+    do
+    {
+        values();
+        printf("'1' to continueeee..:  ");
+        scanf("%d", &flag);
+        printf("------------------------\n\n");
+    } while (flag == 1);
+}
+
+void values()
+{
+
+    double num, rate = 0;
+    int gst;
+    printf("\nenter rate : ");
+    scanf("%lf", &num);
+    printf("enter gst %%: ");
+    scanf("%d", &gst);
     while (1)
     {
-        if (value + (value * 0.12) >= number)
+        if (rate + (rate * (gst / 100.0)) >= num)
         {
-            printf("value is %.2lf\ngst is %.2lf\n", value, value * 0.12);
-            break;
+            printf("value is %.2lf\ngst is %.2lf\n", rate, rate * (gst / 100.0));
+            return;
         }
-        value++;
+        rate++;
     }
 }

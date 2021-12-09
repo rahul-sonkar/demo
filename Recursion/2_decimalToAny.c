a3dc918ba42eb53de0859b924020f5f59469808f
Rahul Sonkar <contect@main-pc.com>
2021-12-09 14:54:00 +0530

    update Dec 9 14:54

--- Recursion/2_decimalToAny.c
+++ Recursion/2_decimalToAny.c
@@ -18,30 +18,9 @@ void decimalToBase(int num, int base)
     {
         tem = num % base;
         decimalToBase(num / base, base);
-        // printf("%d", tem);
-        if (base == 16)
+        if (base == 16 && tem > 9)
         {
-            switch (tem)
-            {
-            case 10:
-                printf("%c", 'A');
-                break;
-            case 11:
-                printf("%c", 'B');
-                break;
-            case 12:
-                printf("%c", 'C');
-                break;
-            case 13:
-                printf("%c", 'D');
-                break;
-            case 14:
-                printf("%c", 'E');
-                break;
-            case 15:
-                printf("%c", 'F');
-                break;
-            }
+            printf("%c", (tem + 55));
         }
         else
         {
